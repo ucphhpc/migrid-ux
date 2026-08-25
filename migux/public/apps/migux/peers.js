39d3c1bb1f3754ef31e46b8b4a8b5d354fd8b1c0
rasmunk <rasmunk@users.noreply.github.com>
2026-08-25 16:26:17 +0200

    Apply format

--- migux/public/apps/migux/peers.js
+++ migux/public/apps/migux/peers.js
@@ -947,7 +947,7 @@ export const App = PeersApp;
         total: 0,
         _error_string: "",
         tooltip: false,
-        _fullname_or_email_tooltip: false
+        _fullname_or_email_tooltip: false,
       },
       peers_requested: {
         ...makePeersListingState({ showColLabel: false, showColKind: false }),
