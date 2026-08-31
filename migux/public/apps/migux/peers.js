a254d56e0cd6c5d3c978536697ba227519776468
rasmunk <rasmunk@users.noreply.github.com>
2026-08-31 15:04:36 +0200

    Refresh the accepted peers results when the peers changed_column changes state

--- migux/public/apps/migux/peers.js
+++ migux/public/apps/migux/peers.js
@@ -273,6 +273,7 @@ export class PeersApp extends AppBase {
 
   peersListingColumnChange(_, namespace) {
     namespace.results("");
+    this.searchAcceptedQuery();
   }
 
   peersListingQuery(endpoint, namespace, additionalParams) {
@@ -520,15 +521,6 @@ export class PeersApp extends AppBase {
           );
         }
       });
-  }
-
-  searchAcceptedSettingsHide(_, namespace) {
-    const wereResultsCleared = namespace.results() === "";
-    if (wereResultsCleared) {
-      // results are cleared when the column seleciton changes, so we infer
-      // infer such a change occurred and thus must be refresh the results
-      this.searchAcceptedQuery();
-    }
   }
 
   /* search requested functions */
