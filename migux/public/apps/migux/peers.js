df821ea0ebe287403174875fa6f5e9bdf2b76366
rasmunk <rasmunk@users.noreply.github.com>
2026-09-17 10:37:45 +0200

    When resetting a new peer it should not change tab

--- migux/public/apps/migux/peers.js
+++ migux/public/apps/migux/peers.js
@@ -783,8 +783,6 @@ export class PeersApp extends AppBase {
     // clear the form values
     const newPeerForm = document.getElementById("peers_new_form");
     newPeerForm.reset();
-    // Switches back to the Accepted Peers Tab on cancel
-    this.changeTab(0);
   }
 
   // edit peer functions
